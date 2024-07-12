63707a2a6a0c8dae870fd25f679411345e0d67f9
JINU <cgzoo9607@gmail.com>
2024-07-12 12:35:07 +0900

    [Fix] Dark Mode IconImage Fixed

---
+++ src/app/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Footer, Header } from '@/components';
+import { ThemeStateProvider } from '@/context/ThemeStateContext';
+
+function MainLayouit({ children }: { children: React.ReactNode }) {
+  return (
+    //redux용
+  <>
+    {/* // <ThemeStateProvider>
+    //   <Header /> */}
+      {children}
+    {/* //   <Footer />
+    // </ThemeStateProvider> */}
+  </>
+  )
+}
+
+export { MainLayouit };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { MainLayouit } from './layouts/MainLayout';
 
 export default function Home() {
   return (
     <main>
-      
+      <MainLayouit>
+1
+      </MainLayouit>
     </main>
   );
 }
