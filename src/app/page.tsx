b3ee1c22527c41f31a3b80a256f116c96ced721e
JINU <cgzoo9607@gmail.com>
2024-08-12 13:51:26 +0900

    [Fix] Fixed Banner Image Show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
+'use client'
+
 import { ImageUploader } from '@/components';
 import { MainLayout } from './layouts/MainLayout';
+import { loadImage } from '@/api/imageLoad';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   // console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  // const [imageUrl, setImageUrl] = useState([]);
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const url = await loadImage();
+        setImageUrl(url);
+        // setImageUrl((prev) => { return [...prev, { url }] });
+      } catch (error) {
+        console.error('Error loading image:', error);
+      }
+    };
+
+    fetchImage();
+  }, []);
+  
   return (
     <main>
       <MainLayout>
-        <ImageUploader />
+        {imageUrl
+          &&  <Image src={imageUrl} width={100} height={100}/>
+        }
+        {/* {imageUrl.length
+          && imageUrl.map((image) => {
+              return (
+                <Image src={image.url} key={image.id} width={100} height={100}/>
+              )
+           })
+        } */}
+        {/* <ImageUploader /> */}
       </MainLayout>
     </main>
   );
