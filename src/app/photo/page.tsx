5e3b3702189ad4305716e43c2953375a6e7f2171
JINU <cgzoo9607@gmail.com>
2024-08-20 18:16:38 +0900

    [Feat] Feature Show Photo List

--- src/app/photo/page.tsx
+++ src/app/photo/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { getModelList } from '@/api/photoApi';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 function PhotoPage() {
   const [modelList, setModelList] = useState([]);
+  const [photoList, setPhotoList] = useState([]);
+
   useEffect(() => {
     getModelList()
       .then((resolve) => setModelList([...resolve.stocks]));
@@ -28,7 +31,31 @@ function PhotoPage() {
         }
       </aside>
       <div className='photo__view'>
-        ss
+        {photoList.length
+          ? 
+            <div>
+            {
+              photoList.map((photo) => {
+                return (
+                  <div key={photo.id}>
+                    <Image
+                      src={''}
+                      // style={{ width: 'auto', height: '100%' }}
+                      layout='intrinsic'
+                      width={500}
+                      height={1380}
+                      alt='bannerImage'
+                      // sizes='100%'
+                    />
+                  </div>
+                )
+              })
+            }
+            </div>
+          : <div>
+            존재하는 사진이 없습니다.
+          </div>
+        }
       </div>
     </main>
   )
